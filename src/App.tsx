2826d3fb2ede60ccb1a578323743a2e1eec390bb
Popov Evgenii <ev.popov@elma-bs.ru>
2024-04-24 20:28:47 +0600

    Edited App component test-7

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,9 @@ import './App.css'
 
 function App() {
 
-    const getText = () => {
-        return 'text';
-    }
-
     return (
         <>
             <h1>Hello world!</h1>
-            <h6>test</h6>
-            <h5>{getText()}</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur culpa debitis delectus, doloremque
-                explicabo facere fugiat harum illo iste laboriosam libero magni nulla optio placeat quisquam ratione
-                similique suscipit. Amet asperiores corporis ea earum enim harum ipsum nostrum officia similique!</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur culpa debitis delectus, doloremque
-                explicabo facere fugiat harum illo iste laboriosam libero magni nulla optio placeat quisquam ratione
-                similique suscipit. Amet asperiores corporis ea earum enim harum ipsum nostrum officia similique!</p>
         </>
     )
 }
